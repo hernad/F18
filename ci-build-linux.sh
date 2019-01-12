9a816168fc5dff08a801af1331d0551bb9b096bd
Ernad Husremovic <hernad@bring.out.ba>
2019-01-12 07:34:08 +0100

    apt-get

--- ci-build-linux.sh
+++ ci-build-linux.sh
@@ -8,9 +8,7 @@ gcc --version
 
 # https://redmine.bring.out.ba/issues/35387
 
-
 export HB_PLATFORM=linux
-
 uname -a
 
 if [ "$BUILD_ARCH" == "ia32" ] ; then
@@ -18,11 +16,9 @@ if [ "$BUILD_ARCH" == "ia32" ] ; then
    #sudo dpkg --add-architecture i386
 
    sudo apt-get -y remove libpq-dev libpq5
-   sudo apt-get install -y g++-multilib gcc-multilib libc6:i386 \
+   sudo apt-get -y install g++-multilib gcc-multilib libc6:i386 \
      libx11-dev:i386 libpcre3-dev:i386 libssl-dev:i386 \
      libncurses5:i386 libstdc++6:i386 lib32stdc++6  libpq-dev:i386 lib32z1
-
-   apt-get -y remove libpq-dev
 
    dpkg -L libpq5:i386
 
