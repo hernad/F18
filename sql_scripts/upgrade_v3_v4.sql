88c4ce97dd7b258cfba8b7184c915cbbf30d00e6
Ernad Husremovic <hernad@bring.out.ba>
2019-03-15 10:58:07 +0100

    fix

--- sql_scripts/upgrade_v3_v4.sql
+++ sql_scripts/upgrade_v3_v4.sql
@@ -41,18 +41,22 @@ DELETE from f18.kalk_kalk where brdok is null or btrim(brdok)='';
 DELETE from f18.kalk_doks where brdok is null or btrim(brdok)='';
 
 DO $$
+DECLARE
+   nRbr numeric;
 BEGIN
-      BEGIN
-        -- check if rbr is char, ako nije STOP => exception
-        select btrim(rbr) from f18.kalk_kalk;
-        alter table f18.kalk_kalk rename column rbr to c_rbr;
-        alter table f18.kalk_kalk add column rbr integer;
-        update f18.kalk_kalk set rbr = to_number(c_rbr, '999') WHERE rbr is NULL;
-        alter table f18.kalk_kalk drop column c_rbr;
 
-  	EXCEPTION WHEN OTHERS THEN
+    -- check if rbr is char, ako nije STOP => exception
+   select to_numeric(rbr, '999') from f18.kalk_kalk
+      INTO nRbr;
+
+   alter table f18.kalk_kalk rename column rbr to c_rbr;
+   alter table f18.kalk_kalk add column rbr integer;
+   update f18.kalk_kalk set rbr = to_number(c_rbr, '999') WHERE rbr is NULL;
+   alter table f18.kalk_kalk drop column c_rbr;
+
+EXCEPTION WHEN OTHERS THEN
           RAISE NOTICE 'rbr is not char';
-  	END;
+
 END;
 $$;
 
