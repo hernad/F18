7b52fbc2551f0fe03d2a2983f86c9855826a7e31
Ernad Husremovic <hernad@bring.out.ba>
2019-03-15 13:50:59 +0100

    replikacija info

---
+++ sql_scripts/replikacija.sql
@@ -0,0 +1,26 @@
+
+--------------------------------------------
+-- KNJIGOVODSTVO      =>   PRODAVNICA
+---------------------------------------------
+--    publisher       ->   subscriber
+----------------------------------------------
+--   f18.valute           f18.valute
+--   f18.partn            f18.partn
+--   f18.tarifa           f18.tarifa
+
+--  p15.pos_knjig         p15.pos_knjig
+--  p15.pos_items_knjig   p15.pos_items_knjig
+
+--  p15.roba              p15.roba
+
+
+
+--------------------------------------------
+-- KNJIGOVODSTVO      <=   PRODAVNICA
+---------------------------------------------
+
+---------------------------------------------
+--   subscriber       <-   publisher
+----------------------------------------------
+--     p15.pos               p15.pos
+--     p15.pos_items         p15.pos_items
