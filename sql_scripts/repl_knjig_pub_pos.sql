782f278c698a69fe3da3630caa6de875960a1fa0
Ernad Husremovic <hernad@bring.out.ba>
2019-03-19 21:34:22 +0100

    fix {{ prod_schema }}

--- sql_scripts/repl_knjig_pub_pos.sql
+++ sql_scripts/repl_knjig_pub_pos.sql
@@ -4,7 +4,7 @@ BEGIN
 CREATE PUBLICATION {{ item.name }}_pos_knjig;
 ALTER PUBLICATION {{ item.name }}_pos_knjig ADD TABLE {{ item.name }}.pos_knjig;
 ALTER PUBLICATION {{ item.name }}_pos_knjig ADD TABLE {{ item.name }}.pos_items_knjig;
-
+ALTER PUBLICATION {{ item.name }}_pos_knjig ADD TABLE {{ item.name }}.roba;
 
 EXCEPTION WHEN OTHERS THEN
    RAISE INFO '{{ item.name }}_pos_knjig publikacija postoji replikaciju postoji';
--- sql_scripts/repl_p_pub.sql
+++ sql_scripts/repl_p_pub.sql
@@ -1,4 +1,14 @@
+
+
+DO $$
+BEGIN
+
 CREATE PUBLICATION {{ prod_schema }}_pos;
-ALTER PUBLICATION {{ prod_schema }}_pos ADD TABLE p16.roba ;
 ALTER PUBLICATION {{ prod_schema }}_pos ADD TABLE p16.pos;
 ALTER PUBLICATION {{ prod_schema }}_pos ADD TABLE p16.pos_items;
+
+EXCEPTION WHEN OTHERS THEN
+   RAISE INFO 'f18_sifre publikacija postoji replikacija';
+END;
+$$;
+
