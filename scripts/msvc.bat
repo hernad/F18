6c943a9df29b43e5f2a5b19524cf3eacfb6e1ae1
Ernad Husremovic <hernad@bring.out.ba>
2019-11-30 15:53:45 +0100

    set msvc

---
+++ scripts/msvc.bat
@@ -0,0 +1,10 @@
+"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Visual C++ Build Tools\Windows Desktop Command Prompts\Visual C++ 2015 x86 Native Build Tools Command Prompt.lnk"
+
+cd \users\hernad\F18
+
+set PATH=d:\x86\hb\bin;%PATH%
+set INCLUDE=d:\x86\hb\include;d:\x86\hb\contrib\hbhpdf\harupdf.ch;%INCLUDE%
+
+set HB_INSTALL_PREFIX=d:\x86\hb
+
+set HB_WITH_PGSQL=d:\x86\postgresql-10.11-1\pgsql\include
