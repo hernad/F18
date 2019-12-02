08777429db366d09c79c3200bdf7c7340e5fc976
hernad <hernad@bring.out.ba>
2019-12-02 10:10:33 +0100

    harbour fb

--- scripts/msvc.bat
+++ scripts/msvc.bat
@@ -16,3 +16,26 @@ REM cd \users\hernad\F18
 
 
 
+REM You should build Harbour this way (or similar)
+REM call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
+REM 
+REM set HB_BUILD_MODE=c
+REM 
+REM set HB_USER_PRGFLAGS=-l-
+REM 
+REM set HB_BUILD_CONTRIBS=yes
+REM 
+REM set HB_WITH_OPENSSL=c:\OpenSSL-Win32\include
+REM 
+REM set HB_WITH_CURL=c:\curl\include
+REM 
+REM del .\src\common\obj\win\msvc64\hbver.obj
+REM 
+REM del .\src\common\obj\win\msvc64\hbver_dyn.obj
+REM 
+REM del .\src\common\obj\win\msvc\hbverdsp.obj
+REM 
+REM win-make.exe
+
+
+
