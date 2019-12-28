308cab94dae99270f4d706e4b9f2f7304a30ef82
hernad <hernad@bring.out.ba>
2019-12-28 22:18:16 +0100

    HB_COMPILER=msvc64

--- build-win32-msvc.bat
+++ build-win32-msvc.bat
@@ -36,11 +36,12 @@ cd %HARBOUR_REPOS_GIT%
 REM x64
 IF [%BINTRAY_ARCH%] EQU [x64] set VCBUILDTOOLS=amd64
 IF [%BINTRAY_ARCH%] EQU [x64] set HARBOUR_BINARIES_ROOT=\users\%USERNAME%\ah\x64\harbour
+IF [%BINTRAY_ARCH%] EQU [x64] set HB_COMPILER=msvc64
 
 REM x86
 IF [%BINTRAY_ARCH%] NEQ [x64] set VCBUILDTOOLS=x86
 IF [%BINTRAY_ARCH%] NEQ [x64] set HARBOUR_BINARIES_ROOT=\users\%USERNAME%\ah\x86\harbour
-
+IF [%BINTRAY_ARCH%] NEQ [x64] set HB_COMPILER=msvc
 
 set VCBUILDTOOLS_PATH="C:\Program Files (x86)\Microsoft Visual C++ Build Tools\vcbuildtools.bat"    
 set LIB_BIN_ROOT=%ROOT_DIR%\3rd\%BUILD_ARCH%
