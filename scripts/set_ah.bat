a54c360c1be8a8af87bfede159e3f65a40aef854
Ernad Husremovic <hernad@bring.out.ba>
2019-12-31 09:11:27 -0800

    windows build

---
+++ scripts/set_ah.bat
@@ -0,0 +1,13 @@
+SET HB_INSTALL_PREFIX=c:\users\hernad\ah\x86\harbour
+SET F18_POS=1
+SET F18_RNAL=1
+SET F18_DEBUG=1
+
+SET PATH=%HB_INSTALL_PREFIX%\bin;%PATH%
+
+
+set HB_DBG_PATH=%HB_DBG%/common;%HB_DBG%/pos;%HB_DBG%/kalk;%HB_DBG%/fin;%HB_DBG%/fakt;%HB_DBG%/os;%HB_DBG%/ld;/%HB_DBG%/epdv;%HB_DBG%/virm;%HB_DBG%/core;%HB_DBG%/core_sql;%HB_DBG%/core_pdf
+
+echo HB_INSTALL_PREFIX=%HB_INSTALL_PREFIX%
+echo HB_DBG_PATH=%HB_DBG%
+
