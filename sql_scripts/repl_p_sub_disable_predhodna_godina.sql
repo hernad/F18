2c5d63eddc0f95d741aacf7dae91d13f2c64b474
hernad <hernad@bring.out.ba>
2020-01-02 18:31:42 +0100

    sub/pub

--- sql_scripts/repl_p_sub_disable_predhodna_godina.sql
+++ sql_scripts/repl_p_sub_disable_predhodna_godina.sql
@@ -1,8 +1,4 @@
 
--- od 2020 ovo treba biti ovako:
--- ALTER SUBSCRIPTION {{ prod_schema }}_f18_sifre_sub_{{ predhodna_godina }} DISABLE;
--- ALTER SUBSCRIPTION {{ prod_schema }}_pos_knjig_sub_{{ predhodna_godina }} DISABLE;
 
--- za sada, radi 2019 mora ovako:
 ALTER SUBSCRIPTION {{ prod_schema }}_f18_sifre_sub DISABLE;
 ALTER SUBSCRIPTION {{ prod_schema }}_pos_knjig_sub DISABLE;
