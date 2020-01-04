afd60ff327736295f89ae121c63aa8e271c8ba85
hernad <hernad@bring.out.ba>
2020-01-04 10:47:16 +0100

    patch f18.metric_metric_id_seq

--- sql_scripts/ansible/patch_knjig.sql
+++ sql_scripts/ansible/patch_knjig.sql
@@ -9,9 +9,9 @@ BEGIN
 
     select MAX(metric_id) + 1 from f18.metric INTO nMax;
 
-    ALTER SEQUENCE serial RESTART WITH nMax;
+    EXECUTE 'ALTER SEQUENCE f18.metric_metric_id_seq RESTART WITH ' || nMax;
 
-    RETURN 0;
 
+   RETURN nMax;
 END;
 $$;
