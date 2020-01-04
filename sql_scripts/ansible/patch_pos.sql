735674f72bd05ea268eb3a2357e485a85423f605
hernad <hernad@bring.out.ba>
2020-01-04 10:53:13 +0100

    patch pos metric

---
+++ sql_scripts/ansible/patch_pos.sql
@@ -0,0 +1,21 @@
+
+
+CREATE OR REPLACE FUNCTION patch_metric_id_seq_pos() RETURNS integer
+LANGUAGE plpgsql
+ AS $$
+DECLARE
+   nMax integer;
+BEGIN
+
+    -- p2.metric
+    select MAX(metric_id) + 1 from {{ item_prodavnica }}.metric INTO nMax;
+    EXECUTE 'ALTER SEQUENCE {{ item_prodavnica }}.metric_metric_id_seq RESTART WITH ' || nMax;
+
+    -- public.metric
+    select MAX(metric_id) + 1 from public.metric INTO nMax;
+    EXECUTE 'ALTER SEQUENCE public.metric_metric_id_seq RESTART WITH ' || nMax;
+
+
+   RETURN nMax;
+END;
+$$;
