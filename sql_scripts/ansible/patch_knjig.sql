d0380898d098780c2834d83a162a15cd87226d01
hernad <hernad@bring.out.ba>
2020-01-04 10:31:58 +0100

    fix

--- sql_scripts/ansible/patch_knjig.sql
+++ sql_scripts/ansible/patch_knjig.sql
@@ -1,4 +1,5 @@
 
+
 CREATE OR REPLACE FUNCTION patch_metric_id_seq() RETURNS integer
 LANGUAGE plpgsql
  AS $$
@@ -6,10 +7,11 @@ DECLARE
    nMax integer;
 BEGIN
 
-    select MAX(metric_id)+1 from f18.metric INTO nMax;
+    select MAX(metric_id) + 1 from f18.metric INTO nMax;
 
     ALTER SEQUENCE serial RESTART WITH nMax;
 
     RETURN 0;
 
 END;
+$$;
