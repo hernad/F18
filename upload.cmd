b7af8280dc9f4ac26da6d5514301791c5113a555
Ernad Husremovic <hernad@bring.out.ba>
2020-07-16 16:00:18 +0200

    windows build_release.cmd && upload.cmd

--- upload.cmd
+++ upload.cmd
@@ -33,12 +33,14 @@ set /p BINTRAY_API_KEY= < %USERPROFILE%\.bintray_api_key
 set HARBOUR_ROOT=c:\dev\harbour\%BUILD_ARCH%\harbour
 
 echo %HARBOUR_ROOT%, bintray: %BINTRAY_OWNER%, %BINTRAY_API_KEY%
-REM upload_bintray_win32.bat
+call upload_bintray_win32.bat
 
 
 if EXIST %CURRENT_DIR%tmp\nul (
   echo rm -rf %CURRENT_DIR%tmp
   c:\cygwin64\bin\rm.exe -rf %CURRENT_DIR%tmp
+) else (
+   echo  dir %CURRENT_DIR%tmp not exists
 )
 
 
