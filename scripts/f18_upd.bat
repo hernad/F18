2eb91eefacceeb9f215c8199f9806703956f990b
Ernad Husremovic <hernad@bring.out.ba>
2021-04-09 09:46:49 +0200

    f18_update - postgresql utilities

--- scripts/f18_upd.bat
+++ scripts/f18_upd.bat
@@ -1,17 +1,18 @@
 @echo off
-REM # ver 1.0.3
-REM # bjasko@bring.out.ba
-REM # date 09.02.2021
+REM # ver 2.0.0
+REM # hernad@bring.out.ba
+REM # date 09.04.2021
 set PATH=%PATH%;C:\knowhowERP\bin;C:\knowhowERP\lib;C:\knowhowERP\util
 set DEST=C:\knowhowERP\bin
 set UTIL=C:\knowhowERP\util
-set PSQLVER=11.0.0
+set KLIB=C:\knowhowERP\lib
+set PSQLVER=12.1
 set URL=http://download.bring.out.ba/F18_v3
 
 :SERVICE
 echo.
 echo.
-echo "Provjeravam dali je F18 zatvoren"
+echo Provjera da li je F18 zatvoren
 echo.
 echo.
 
@@ -27,7 +28,7 @@ if not exist %1  goto ERR1
 
 echo.
 echo.
-echo "Provjera ispravnosti arhive"
+echo Provjera ispravnosti arhive
 echo.
 echo.
 gzip -tv  %1
@@ -37,12 +38,12 @@ if errorlevel 1 goto ERR2 if errorlevel 0 goto OK
 
 gzip -dNfc  < %1 > %DEST%\F18.exe
 del /Q  %1
-goto END
+goto PSQL
 
 :ERR1
 echo.
 echo.
-echo "Problem sa F18 update fajlom, Prekidam operaciju UPDATE-a"
+echo Problem sa F18 update fajlom, Prekidam operaciju UPDATE-a
 echo.
 echo.
 pause
@@ -52,60 +53,112 @@ rem exit
 
 echo.
 echo.
-echo "Greska unutar F18 update fajla, Prekidam operaciju, ponovite UPDATE"
+echo Greska unutar F18 update fajla, Prekidam operaciju, ponovite UPDATE
 echo.
 echo.
 pause
 rem exit
 
-:END
+:PSQL
 
+IF NOT EXIST %UTIL%\psql.exe goto UPSQL
 %UTIL%\psql.exe --version | %UTIL%\grep.exe "%PSQLVER%"
 
-IF "%ERRORLEVEL%"=="0" GOTO END2
+IF "%ERRORLEVEL%"=="0" GOTO END
 
 echo upgrade psql utilities
 
-REM libcrypto-1_1.dll.gz
-REM libiconv.dll.gz
-REM libpq.dll.gz
-REM libssl-1_1.dll.gz
-REM libxml2.dll.gz
-REM pg_dump.exe.gz
-REM pg_restore.exe.gz
-REM psql.exe.gz
-REM zlib1.dll.gz
+del /Q %KLIB%\psql.exe
+del /Q %KLIB%\pg_dump.exe
+del /Q %KLIB%\pg_dumpall.exe
 
+del /Q %UTIL%\psql.exe
+del /Q %UTIL%\pg_dump.exe
+del /Q %UTIL%\pg_dumpall.exe
 
-REM %UTIL%\gzip -tv  
-REM if errorlevel 1 goto ERR2 if errorlevel 0 goto OK
 set FILE=libcrypto-1_1.dll
-%UTIL%\wget %URL%/%FILE%.gz -o %FILE%.gz
-%UTIL%\gzip -dNfc  < %FILE%.gz > %UTIL%\%FILE%.gz
+%UTIL%\wget -qO %FILE%.gz %URL%/%FILE%.gz
+%UTIL%\gzip -tv %FILE%.gz
+if NOT "%ERRORLEVEL%"=="0" GOTO GZERR   
+%UTIL%\gzip -dNfc  < %FILE%.gz > %UTIL%\%FILE%
 del /Q %FILE%.gz 
 
-set FILE=libiconv.dll.gz
-%UTIL%\wget %URL%/%FILE%.gz -o %FILE%.gz
-%UTIL%\gzip -dNfc  < %FILE%.gz > %UTIL%\%FILE%.gz
+set FILE=libiconv.dll
+%UTIL%\wget -qO %FILE%.gz %URL%/%FILE%.gz 
+%UTIL%\gzip -tv %FILE%.gz
+if NOT "%ERRORLEVEL%"=="0" GOTO GZERR
+%UTIL%\gzip -dNfc  < %FILE%.gz > %UTIL%\%FILE%
 del /Q %FILE%.gz 
 
-set FILE=libpq.dll.gz
-%UTIL%\wget %URL%/%FILE%.gz -o %FILE%.gz
-%UTIL%\gzip -dNfc  < %FILE%.gz > %UTIL%\%FILE%.gz
+set FILE=libpq.dll
+%UTIL%\wget -qO %FILE%.gz %URL%/%FILE%.gz
+%UTIL%\gzip -tv %FILE%.gz
+if NOT "%ERRORLEVEL%"=="0" GOTO GZERR
+%UTIL%\gzip -dNfc  < %FILE%.gz > %UTIL%\%FILE%
 del /Q %FILE%.gz
 
-set FILE=libssl-1_1.dll.gz
-%UTIL%\wget %URL%/%FILE%.gz -o %FILE%.gz
-%UTIL%\gzip -dNfc  < %FILE%.gz > %UTIL%\%FILE%.gz
+set FILE=libssl-1_1.dll
+%UTIL%\wget -qO %FILE%.gz %URL%/%FILE%.gz
+%UTIL%\gzip -tv %FILE%.gz
+if NOT "%ERRORLEVEL%"=="0" GOTO GZERR 
+%UTIL%\gzip -dNfc  < %FILE%.gz > %UTIL%\%FILE%
 del /Q %FILE%.gz
 
-:END2
+set FILE=libxml2.dll
+%UTIL%\wget -qO %FILE%.gz %URL%/%FILE%.gz
+%UTIL%\gzip -tv %FILE%.gz
+if NOT "%ERRORLEVEL%"=="0" GOTO GZERR 
+%UTIL%\gzip -dNfc  < %FILE%.gz > %UTIL%\%FILE%
+del /Q %FILE%.gz
+
+set FILE=zlib1.dll
+%UTIL%\wget -qO %FILE%.gz %URL%/%FILE%.gz
+%UTIL%\gzip -tv %FILE%.gz
+if NOT "%ERRORLEVEL%"=="0" GOTO GZERR 
+%UTIL%\gzip -dNfc  < %FILE%.gz > %UTIL%\%FILE%
+del /Q %FILE%.gz
+
+set FILE=pg_dump.exe
+%UTIL%\wget -qO %FILE%.gz %URL%/%FILE%.gz
+%UTIL%\gzip -tv %FILE%.gz
+if NOT "%ERRORLEVEL%"=="0" GOTO GZERR 
+%UTIL%\gzip -dNfc  < %FILE%.gz > %UTIL%\%FILE%
+del /Q %FILE%.gz
+
+set FILE=pg_dumpall.exe
+%UTIL%\wget -qO %FILE%.gz %URL%/%FILE%.gz
+%UTIL%\gzip -tv %FILE%.gz
+if NOT "%ERRORLEVEL%"=="0" GOTO GZERR 
+%UTIL%\gzip -dNfc  < %FILE%.gz > %UTIL%\%FILE%
+del /Q %FILE%.gz
+
+set FILE=pg_restore.exe
+%UTIL%\wget -qO %FILE%.gz %URL%/%FILE%.gz
+%UTIL%\gzip -tv %FILE%.gz
+if NOT "%ERRORLEVEL%"=="0" GOTO GZERR 
+%UTIL%\gzip -dNfc  < %FILE%.gz > %UTIL%\%FILE%
+del /Q %FILE%.gz
+
+set FILE=psql.exe
+%UTIL%\wget -qO %FILE%.gz %URL%/%FILE%.gz
+%UTIL%\gzip -tv %FILE%.gz
+if NOT "%ERRORLEVEL%"=="0" GOTO GZERR 
+%UTIL%\gzip -dNfc  < %FILE%.gz > %UTIL%\%FILE%
+del /Q %FILE%.gz
+
+goto end
+
+:GZERR
+echo ERROR wget gunzip
+goto :eof
+
+:END
 echo.
 echo.
-echo "Update je zavrsen uspjesno"
+echo Update je zavrsen uspjesno
 echo.
-echo "Mozete zatvoriti ovaj prozor te ponovo pokrenuti F18"
+echo Mozete zatvoriti ovaj prozor te ponovo pokrenuti F18
 echo.
 echo.
 pause
-rem exit
+
