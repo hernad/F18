95a6e1b7d8c1606bf987880685533311845bd83b
Ernad Husremovic <hernad@bring.out.ba>
2021-04-13 14:58:29 +0200

    upload rsync

--- upload.sh
+++ upload.sh
@@ -8,20 +8,20 @@ if [ -d tmp ] ; then
   rm -rf tmp
 fi
 
-if [ ! -f $HOME/.bintray_owner ]; then
-   echo potreban fajl $HOME/.bintray_owner
-   exit 1
-fi
+#if [ ! -f $HOME/.bintray_owner ]; then
+#   echo potreban fajl $HOME/.bintray_owner
+#   exit 1
+#fi
 
-if [ ! -f $HOME/.bintray_api_key ]; then
-   echo potreban fajl $HOME/.bintray_api_key
-   exit 1
-fi
+#if [ ! -f $HOME/.bintray_api_key ]; then
+#   echo potreban fajl $HOME/.bintray_api_key
+#   exit 1
+#fi
 
 
 
-export BINTRAY_OWNER=`cat $HOME/.bintray_owner`
-export BINTRAY_API_KEY=`cat $HOME/.bintray_api_key`
+#export BINTRAY_OWNER=`cat $HOME/.bintray_owner`
+#export BINTRAY_API_KEY=`cat $HOME/.bintray_api_key`
 
 export HB_ROOT=$HOME/harbour
 
@@ -29,7 +29,16 @@ echo "$HB_ROOT, bintray: $BINTRAY_OWNER, $BINTRAY_API_KEY"
 
 #read
 
-./upload_bintray_linux.sh
+#./upload_bintray_linux.sh
+BUILD_ARCH=x64
+F18_PACKAGE=F18-linux-$BUILD_ARCH
+#F18_VERSION=${BUILD_BUILDNUMBER}
+F18_VERSION=`echo "const json=require('./package.json') ; console.log(json.f18)" | node`
+
+FILE=${F18_PACKAGE}_${F18_VERSION}.zip
+echo "upload: ${F18_PACKAGE} / ${FILE} root@192.168.168.251:/var/www/html/F18/, root@192.168.168.252:/var/www/html/F18/"
+rsync -avz $FILE root@192.168.168.251:/var/www/html/F18/
+rsync -avz $FILE root@192.168.168.252:/var/www/html/F18/
 
 
 echo delete $CURRENT_DIR/tmp ...
