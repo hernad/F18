c7b041d7631e1bd2a3b202ac408abfc1e8cb37e1
Ernad Husremovic <hernad@bring.out.ba>
2021-05-11 14:07:05 +0200

    from_prg_to_ziher.py

---
+++ BUILD
@@ -0,0 +1,247 @@
+load("@rules_cc//cc:defs.bzl", "cc_library", "cc_binary")
+load("//bazel:shared_library.bzl", "shared_library")
+load("//bazel:zh_comp.bzl", "zh_comp_all")
+load("//bazel:variables.bzl", "C_OPTS", "ZH_COMP_OPTS", 
+    "ZH_Z18_COMP_OPTS", "L_OPTS", "L_OPTS_2",
+     "POSTGRESQL_HEADERS", "POSTGRESQL_COPT", "POSTGRESQL_LIB", "WINDOWS_LINUX" )
+
+
+ZH_COMP_OPTS_F18=[
+    "-n",
+    "-izh_zero", 
+    "-izh_rtl",
+    "-izh_rtl/gt",
+    "-iF18/include",
+    #"-iF18/fin", #enabavke_eisporuke.zhh
+    "-izh_harupdf",
+    "-Ithird_party/harupdf",
+    "-Ithird_party/xlsxwriter",
+    "-DGT_DEFAULT_CONSOLE",
+    "-DELECTRON_HOST",
+    #"-DGT_DEFAULT_GUI",
+    "-DF18_POS",
+    #"-DF18_DEBUG",
+    #"-b" no debug
+]
+
+ZH_DEPS_F18=[ 
+    "//zh_zero:headers_filegroup", 
+    "//zh_rtl:headers_filegroup",
+    "//F18/include:headers_filegroup",
+    #"//F18/fin:headers_filegroup",
+    "//zh_harupdf:headers_filegroup"
+]
+
+
+F18_LIB = "klijent"
+
+
+cc_binary(
+    name = "F18-klijent",
+    srcs = [ "F18-klijent.c" ],
+    deps = select({
+            "//bazel:windows": [ "//zh_zero:headers", ":F18_dll_import", ":ziher_dll_import"], 
+            "//conditions:default": [ "//zh_zero:headers", ":F18_import", ":ziher_import"],
+        }),  #+ POSTGRESQL_LIB
+    linkopts = L_OPTS + L_OPTS_2,
+    copts = [
+        "-Izh_zero",
+        "-DZH_DYNIMP",
+        "-DZH_TR_LEVEL=4",
+    ],
+    #linkstatic = True,
+    visibility = ["//visibility:public"],
+)
+
+
+shared_library(
+    name = "F18",
+    os = "windows",
+    srcs = [ ":F18_zh" ],
+    hdrs = glob([
+        "*.h",
+        "*.zhh",
+    ]) + POSTGRESQL_HEADERS,
+    deps = ["//zh_zero:headers", "//zh_rtl:headers", ":ziher_dll_import"] + POSTGRESQL_LIB,
+    linkopts = L_OPTS + L_OPTS_2,
+    copts = [
+        "-Izh_zero",
+        "-Izh_vm",
+        "-Izh_rtl",
+        "-DZH_DYNLIB",
+        #"-DZH_TR_LEVEL=4", #INFO
+        #"-DZH_TR_LEVEL=5", DEBUG
+    ]  + POSTGRESQL_COPT,
+    visibility = ["//visibility:public"],
+    exec_compatible_with = [
+        #"@platforms//cpu:x86_64",
+        "@platforms//os:windows",
+    ],
+)
+
+shared_library(
+    name = "F18",
+    os = "linux",
+    srcs = [ ":F18_zh" ],
+    hdrs = glob([
+        "*.h",
+        "*.zhh",
+    ]) + POSTGRESQL_HEADERS,
+    deps = [ "//zh_zero:headers", "//zh_rtl:headers", ":ziher_import"] + POSTGRESQL_LIB,
+    linkopts = L_OPTS + L_OPTS_2,
+    copts = [
+        "-Izh_zero",
+        "-Izh_vm",
+        "-Izh_rtl",
+        "-DZH_DYNLIB",
+        #"-DZH_TR_LEVEL=4", #INFO
+        #"-DZH_TR_LEVEL=5", DEBUG
+    ]  + POSTGRESQL_COPT,
+    visibility = ["//visibility:public"],
+    exec_compatible_with = [
+        "@platforms//cpu:x86_64",
+        "@platforms//os:linux",
+    ],
+)
+
+_ZIHER_SRCS = [ 
+       "//zh_zero:c_sources",
+       "//zh_vm:c_sources", 
+       "//zh_vm:zh_vm_zh",
+       "//zh_macro:c_sources",
+       "//zh_rtl:c_sources",
+       "//zh_rtl:zh_rtl_zh",
+       "//zh_rtl/gt:c_sources",
+       "//zh_rtl/gt:zh_rtl_gt_zh",
+       "//zh_rtl/rdd:c_sources",
+       "//zh_rtl/rdd:zh_rtl_rdd_zh",
+       "//zh_xlsxwriter:c_sources",
+       "//zh_xlsxwriter:zh_xlsxwriter_zh",
+       "//zh_harupdf:c_sources",
+       "//zh_harupdf:zh_harupdf_zh",
+       "//zh_tools:zh_tools_zh",
+       "//zh_tools:c_sources",
+       "//zh_minizip:zh_minizip_zh",
+       "//zh_minizip:c_sources",
+       "//zh_pgsql:zh_pgsql_zh",
+       "//zh_pgsql:c_sources",
+       "//zh_tcp_ip:c_sources",
+       "//zh_tcp_ip:zh_tcp_ip_zh",
+       "//third_party/zlib:c_sources",
+       "//third_party/pcre2:c_sources",
+       "//third_party/xlsxwriter:c_sources",
+       "//third_party/harupdf:c_sources",
+       "//third_party/png:c_sources",
+       "//third_party/minizip:c_sources",
+    ]
+
+_ZIHER_DEPS = [ 
+        "//zh_zero:headers",
+        "//zh_vm:headers",
+        "//zh_macro:headers",
+        "//zh_comp:headers",
+        "//zh_rtl:headers",
+        "//zh_rtl/gt:headers",
+        "//zh_rtl/rdd:headers",
+        "//zh_xlsxwriter:headers",
+        "//zh_harupdf:headers",
+        "//zh_tools:headers",
+        "//zh_minizip:headers",
+        "//zh_pgsql:headers",
+        "//third_party/zlib:headers",
+        "//third_party/pcre2:headers",
+        "//third_party/harupdf:headers",
+        "//third_party/xlsxwriter:headers",
+        "//third_party/png:headers",
+        "//third_party/minizip:headers",
+    ] + POSTGRESQL_LIB
+
+_ZIHER_COPTS = [
+        "-DUNICODE", # gt_wvt
+        "-Izh_zero",
+        "-Izh_vm",
+        "-Izh_rtl",
+        "-Izh_harupdf",
+        "-Izh_rtl/gt",
+        "-Izh_rtl/rdd/rdd_sql",
+        "-Ithird_party/zlib",
+        "-Ithird_party/minizip",
+        "-DCREATE_LIB", # minizip no main
+        "-Ithird_party/pcre2",
+        "-DSUPPORT_UNICODE", #pcre2
+        "-DHAVE_CONFIG_H", #pcre2
+        "-DPCRE2_CODE_UNIT_WIDTH=8", #pcre2
+        "-Ithird_party/harupdf",
+        "-Ithird_party/xlsxwriter",
+        "-DUSE_STANDARD_TMPFILE", # xlsxwriter don't use tmpfileplus
+        "-Ithird_party/png",
+        "-DZH_DYNLIB",
+        #"-DZH_TR_LEVEL=4", #INFO
+        #"-DZH_TR_LEVEL=5", DEBUG
+    ]  + POSTGRESQL_COPT
+
+shared_library(
+    name = "ziher",
+    os = "windows",
+    srcs = _ZIHER_SRCS,
+    hdrs = glob([
+        "*.h",
+        "*.zhh"
+    ]) + POSTGRESQL_HEADERS,
+    deps = _ZIHER_DEPS,
+    linkopts = L_OPTS + L_OPTS_2,
+    copts = _ZIHER_COPTS,
+    visibility = ["//visibility:public"],
+    exec_compatible_with = [
+        #"@platforms//cpu:x86_64",
+        "@platforms//os:windows",
+    ],
+)
+
+shared_library(
+    name = "ziher",
+    os = "linux",
+    srcs = _ZIHER_SRCS,
+    hdrs = glob([
+        "*.h",
+        "*.zhh"
+    ]) + POSTGRESQL_HEADERS,
+    deps = _ZIHER_DEPS,
+    linkopts = L_OPTS + L_OPTS_2 + ["-lX11"],
+    copts = _ZIHER_COPTS,
+    visibility = ["//visibility:public"],
+    exec_compatible_with = [
+        "@platforms//cpu:x86_64",
+        "@platforms//os:linux",
+    ],
+)
+
+zh_comp_all(
+    name = "F18_zh", 
+    srcs = glob([ 
+        "*.zh",
+        "common/*.zh",
+        "common_legacy/*.zh",
+        "core/*.zh",
+        "core_dbf/*.zh",
+        "core_pdf/*.zh",
+        "core_reporting/*.zh",
+        "core_semafori/*.zh",
+        "core_sql/*.zh",
+        "core_string/*.zh",
+        "core_ui2/*.zh",
+        "fin/*.zh",
+        "kalk/*.zh",
+        "kalk_legacy/*.zh",
+        "fakt/*.zh",
+        "fiskalizacija/*.zh",
+        "ld/*.zh",
+        "pos/*.zh",
+        "os/*.zh",
+        "virm/*.zh",
+        "epdv/*.zh",
+    ]),
+    args = ZH_COMP_OPTS_F18,
+    deps = ZH_DEPS_F18,
+    visibility = ["//visibility:public"],
+)
---
+++ scripts/from_prg_to_ziher.py
@@ -0,0 +1,149 @@
+import os
+
+def ext_prgch_zhzhh(file_name):
+    base = os.path.splitext(file_name)[0]
+    if os.path.splitext(file_name)[1] == '.prg':
+        os.rename(file_name, base + '.zh')
+        print('prg -> ' +  base + '.zh')
+    if os.path.splitext(file_name)[1] == '.ch':
+        os.rename(file_name, base + '.zhh')
+        print('ch -> ' +  base + '.zhh')
+
+
+
+def convert_content_hb_zh(file_name):
+    import re
+
+    if not os.path.splitext(file_name)[1] in ['.zh', '.zhh']:
+        # only .zh, .zhh convert
+        return
+
+    if os.path.exists(file_name + '.cnv'):
+        os.remove(file_name + '.cnv')
+    os.rename(file_name, file_name + '.cnv')
+    output_file = open(file_name, "w")
+
+    with open (file_name + '.cnv', 'r' ) as f:
+        content = f.read()
+        #content_new = re.sub('(\d{2}|[a-yA-Y]{3})\/(\d{2})\/(\d{4})', r'\1-\2-\3', content, flags = re.M)
+        content_new = re.sub('#include "f18.ch"', '#include "f18.zhh"', content, flags = re.M)
+        #hb_default()
+        content_new = re.sub('hb_([a-zA-Z]+)', r'zh_\1', content_new, flags = re.M)
+        #HB_GTI_DESKTOPWIDTH
+        content_new = re.sub('HB_([_a-zA-Z]+)', r'ZH_\1', content_new, flags = re.M)
+
+        # promjena imena funkcija hb -> ziher
+        # https://docs.python.org/3/library/re.html
+        content_new = re.sub('zh_cdpSelect', 'codepageSelect', content_new, flags = re.M | re.IGNORECASE)
+        content_new = re.sub('zh_cdpOS\(', 'codepageOS(', content_new, flags = re.M | re.IGNORECASE)
+        
+        content_new = re.sub('zh_ps\(', 'PathSeparator(', content_new, flags = re.M | re.IGNORECASE)
+        # subst( => substr(
+        content_new = re.sub('subst\(', 'substr(', content_new, flags = re.M | re.IGNORECASE)
+        content_new = re.sub('TRANS\s*\(', 'TRANSFORM(', content_new, flags = re.M | re.IGNORECASE)
+        content_new = re.sub('REPL\s*\(', 'Replicate(', content_new, flags = re.M | re.IGNORECASE)
+
+        # don't use SetKXLat
+        content_new = re.sub('SetKXLat\(', '// SetKXLat(', content_new, flags = re.M | re.IGNORECASE)
+
+        # zh_ValToStr => ValToStr(
+        content_new = re.sub('zh_ValToStr\(', 'ValToStr(', content_new, flags = re.M | re.IGNORECASE)
+
+
+        #include "setcurs.ch"
+        content_new = re.sub('"setcurs.ch"', '"set_curs.zhh"', content_new, flags = re.M)
+        #include "rddsys.ch"
+        content_new = re.sub('"rddsys.ch"', '"rdd_sys.zhh"', content_new, flags = re.M)
+        #include "hbusrrdd.ch"
+        content_new = re.sub('"hbusrrdd.ch"', '"rdd_usr.zhh"', content_new, flags = re.M)
+        #include "fileio.ch"
+        content_new = re.sub('"fileio.ch"', '"file_io.zhh"', content_new, flags = re.M)
+        #include "error.ch"
+        content_new = re.sub('"error.ch"', '"error.zhh"', content_new, flags = re.M)
+        #include "dbstruct.ch"
+        content_new = re.sub('"dbstruct.ch"', '"db_struct.zhh"', content_new, flags = re.M)
+        #include "common.ch"
+        content_new = re.sub('"common.ch"', '"common.zhh"', content_new, flags = re.M)
+        #include "getexit.ch"
+        content_new = re.sub('"getexit.ch"', '"get_exit.zhh"', content_new, flags = re.M)
+        #include "inkey.ch"
+        content_new = re.sub('"inkey.ch"', '"inkey.zhh"', content_new, flags = re.M)
+        #include "fmk.ch"
+        content_new = re.sub('"fmk.ch"', '"fmk.zhh"', content_new, flags = re.M)
+        #include "rt_vars.ch"
+        content_new = re.sub('#include "rt_vars.ch"', '// #include "rt_vars.zhh"', content_new, flags = re.M)
+        content_new = re.sub('#include "rt_main.ch"', '// #include "rt_main.zhh"', content_new, flags = re.M)
+        #include "hbthread.ch"
+        content_new = re.sub('"hbthread.ch"', '"thread.zhh"', content_new, flags = re.M)
+        content_new = re.sub('"hbgtinfo.ch"', '"gt_info.zhh"', content_new, flags = re.M)
+        content_new = re.sub('"set.ch"', '"set.zhh"', content_new, flags = re.M)
+        content_new = re.sub('"box.ch"', '"box.zhh"', content_new, flags = re.M)
+     
+        content_new = re.sub('"dbedit.ch"', '"dbedit.zhh"', content_new, flags = re.M)
+        content_new = re.sub('"achoice.ch"', '"achoice.zhh"', content_new, flags = re.M)
+        content_new = re.sub('"rddsql.ch"', '"rdd_sql.zhh"', content_new, flags = re.M)
+        content_new = re.sub('"dbinfo.ch"', '"db_info.zhh"', content_new, flags = re.M)
+        content_new = re.sub('"pdf_cls.ch"', '"pdf_class.zhh"', content_new, flags = re.M)
+        content_new = re.sub('"memoedit.ch"', '"memo_edit.zhh"', content_new, flags = re.M)
+
+
+        content_new = re.sub('"o_f18.ch"', '"o_f18.zhh"', content_new, flags = re.M)
+        content_new = re.sub('"f_f18.ch"', '"f_f18.zhh"', content_new, flags = re.M)
+        content_new = re.sub('"f18_separator.ch"', '"f18_separator.zhh"', content_new, flags = re.M)
+        content_new = re.sub('"f18_release.ch"', '"f18_release.zhh"', content_new, flags = re.M)
+        content_new = re.sub('"f18_rabat.ch"', '"f18_rabat.zhh"', content_new, flags = re.M)
+        content_new = re.sub('"f18_request.ch"', '"f18_request.zhh"', content_new, flags = re.M)
+        content_new = re.sub('"f18_cre_all.ch"', '"f18_cre_all.zhh"', content_new, flags = re.M)
+        content_new = re.sub('#require "sddpg.ch"', '// #require "sddpg.zhh"', content_new, flags = re.M)
+
+        #include "f18_color.ch"
+        content_new = re.sub('"f18_color.ch"', '"f18_color.zhh"', content_new, flags = re.M)
+        #include "hbclass.ch"
+        content_new = re.sub('"hbclass.ch"', '"class.zhh"', content_new, flags = re.M)
+        #include "f18_ver.ch"
+        content_new = re.sub('"f18_ver.ch"', '"f18_ver.zhh"', content_new, flags = re.M)
+        #include "simpleio.ch"
+        content_new = re.sub('"simpleio.ch"', '"simple_io.zhh"', content_new, flags = re.M)
+        #include "enabavke_eisporuke.ch"
+        content_new = re.sub('"enabavke_eisporuke.ch"', '"enabavke_eisporuke.zhh"', content_new, flags = re.M)
+        #include "hbapi.h"
+        content_new = re.sub('#include "hbapi.h"', '#include "zh_api.h"', content_new, flags = re.M)
+        #include "hbapifs.h"
+        content_new = re.sub('#include "hbapifs.h"', '#include "zh_fs_api.h"', content_new, flags = re.M)
+        #include "hbapierr.h"
+        content_new = re.sub('#include "hbapierr.h"', '#include "zh_error_api.h"', content_new, flags = re.M)
+        #include "hbapigt.h"
+        content_new = re.sub('#include "hbapigt.h"', '#include "zh_gt_api.h"', content_new, flags = re.M)
+        #include "hbapiitm.h"
+        content_new = re.sub('#include "hbapiitm.h"', '#include "zh_item_api.h"', content_new, flags = re.M)
+        
+        # HBEditor
+        content_new = re.sub('HBEditor', 'ZHEditor', content_new, flags = re.M)
+        output_file.write(content_new)
+        
+
+    
+def walk_1():
+    print("================ convert prg, ch => zh, zhh ==================================")
+    for root, dirs, files in os.walk(".", topdown=True):
+        for name in files:
+            print("file:", os.path.join(root, name))
+            ext_prgch_zhzhh(os.path.join(root, name))
+        for name in dirs:
+            print("dir:", os.path.join(root, name))
+
+
+
+#walk_1()
+
+
+def walk_2():
+    print("================ convert content hb_ -> zh_ ==================================")
+    for root, dirs, files in os.walk(".", topdown=True):
+        for name in files:
+            print("file:", os.path.join(root, name))
+            convert_content_hb_zh(os.path.join(root, name))
+        for name in dirs:
+            print("dir:", os.path.join(root, name))
+
+walk_2()
