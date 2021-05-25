87caf08c8136730a99c22f8cf27b40cd99280941
Ernad Husremovic <hernad@bring.out.ba>
2021-05-25 19:05:03 +0200

    update py

---
+++ F18-klijent.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#define SHARED_LIB
+
+#include "zh_vm_pub.h"
+#include "zh_trace.h"
+//#include "zh_pcode.h"
+#include "zh_init.h"
+//#include "zh_xvm.h"
+
+#if defined( _MSC_VER )
+#include <windows.h>
+#endif
+
+#if defined( SHARED_LIB )
+int run_main( int argc, char * argv[] );
+#else
+int main( int argc, char * argv[] );
+#endif
+
+void simple_printf(const char* fmt, ...);
