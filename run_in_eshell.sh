ce429950540eb86e5db4eb946a2e85231d92c002
hernad <hernad@bring.out.ba>
2024-09-13 11:05:41 +0200

    centos7 dev codium

--- run_in_eshell.sh
+++ run_in_eshell.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-F18_INSTALLED_ALREADY=`ls -d $HOME/.eShell-dev/extensions/F18/F18-linux-x64-*`
+
+ESHELL_F18_DIR="$HOME/.eShell-dev/extensions/F18"
+
+# codium
+ESHELL_F18_DIR="$HOME/.vscode-oss/extensions/F18"
+
+F18_INSTALLED_ALREADY=`ls -d $ESHELL_F18_DIR/F18-linux-x64-*`
 
 CURRENTDIR=`pwd`
 
@@ -12,7 +18,7 @@ echo "eShell-dev F18 provjera nove verzije iskljuciti !"
 PATH=$F18_INSTALLED_ALREADY:$PATH
 #konflikt sa eShell openssl LD_LIBRARY_PATH=$F18_INSTALLED_ALREADY:$LD_LIBRARY_PATH
 
-F18_0_DIR=$HOME/.eShell-dev/extensions/F18/F18_0
+F18_0_DIR=$ESHELL_F18_DIR/F18_0
 
 for f in libcrypto.so libcurl.so libpq.so libssl.so libz.so
 do
@@ -32,20 +38,10 @@ chmod +w ${F18_0_DIR}/F18-klijent
 cp -av F18-klijent ${F18_0_DIR}/F18-klijent
 
 
-cd ../eShell
-LD_LIBRARY_PATH= scripts/code.sh
+#cd ../eShell
+#LD_LIBRARY_PATH= scripts/code.sh
 
-#set Token=ESHELL_%RANDOM%
 
-#start "%Token%" cmd /c scripts\code.bat
+#cd $CURRENTDIR
 
-#:waiteShell
-#ping -n 2 localhost >nul 2>nul
-#tasklist /fi "WINDOWTITLE eq %Token%" | findstr "cmd" >nul 2>nul && set Child1=1 || set Child1=
-#if not defined Child1 goto endloop
-#goto waiteShell
-
-#:endloop
-#echo eShell-dev died
-
-cd $CURRENTDIR
+codium
